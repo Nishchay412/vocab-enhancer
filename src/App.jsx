524f5fec0e164ac065c49ac13c2c4d205d35cebb
Nishchay Ranjan <nishchay1023@MacBook-Air.local>
2024-08-26 13:49:37 -0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "./index.css"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' font-bold text-4xl'> 
+        hello
+</div>
+
+       
+      
+    </>
+  )
+}
+
+export default App
